7c9ea9b8ab8bb1ba37b38e22ef17ec733393ac15
stuximan <felipeeusoudoscosta@gmail.com>
2026-02-04 16:45:09 -0300

    adicionando front end na api

--- apibanco.py
+++ apibanco.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import sqlite3
 import re
 
 app = Flask(__name__)
+CORS(app)
 DATABASE = "individuos.db"
 
 # ---------------------------
